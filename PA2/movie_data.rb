ce1638f5e7edb9e3fa708f77aceb881ad04bef83
aidangainor <againor@brandeis.edu>
2016-02-01 16:21:43 -0500

    added folder

--- PA2/movie_data.rb
+++ PA2/movie_data.rb
@@ -5,6 +5,7 @@ class MovieData
     #It is optional, but preferred to indicate the training / test pair as well as a symbol
 
     #Do some housekeeping to load in files into hashmaps
+    load "./movie_test.rb"
     if prefix_set == nil
       bases = load_data(directory + "//" + "u.data")
       tests = [nil, nil]
@@ -151,60 +152,6 @@ class MovieData
       end
     end
     return movie_test
-  end
-end
-
-
-class MovieTest
-  #MovieTest holds prediction/test data and basic statistical operations
-  #It has methods to calculate mean error, stddev of error, and rms of error
-  def initialize
-    @predictions = []
-  end
-
-  def add_prediction(u, m, r, p)
-    #Adds a row of data to our internal array
-    @predictions.push([u, m, r, p])
-  end
-
-  def mean
-    #Calculate the mean of the error
-    sum = 0.0
-    count = 0.0
-    @predictions.each do |array|
-      sum += (array[2] - array[3]).abs
-      count += 1
-    end
-
-    return sum/count
-  end
-
-  def stddev
-    #Calculate standard deviation of error
-    mean_error = mean()
-    sum = 0.0
-    @predictions.each do |array|
-      error = (array[2] - array[3]).abs
-      sum += (error - mean_error)**2
-    end
-    variance = sum / (@predictions.length - 1)
-    return variance ** 0.5
-  end
-
-  def rms
-    #Calculate root mean square of error
-    sum = 0.0
-    @predictions.each do |array|
-      sum += (array[2] - array[3])**2
-    end
-    variance = sum / @predictions.length
-    return variance ** 0.5
-
-  end
-
-  def to_a
-    #Return the predictions / test data, which is just our internal array
-    return @predictions
   end
 end
 
